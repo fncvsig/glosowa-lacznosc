d9a1a640d8823ce1d0a60352af9aa1ff2a175002
fncvsig <45051422+fncvsig@users.noreply.github.com>
2019-04-01 14:33:00 +0200

    add data validation

---
+++ dataValidation.py
@@ -0,0 +1,22 @@
+# Checks if all data has the correct framerate
+
+import os
+import wave
+
+frameRate = 8000;
+clear = 1;
+
+for subdir, dirs, files in os.walk('data'):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        if filepath.endswith(".wav"):
+            wf = wave.open(filepath, 'rb')
+
+            if wf.getframerate() != frameRate:
+                print("Incorrect frame rate in: ", filepath, "(", wf.getframerate(), "instead of", frameRate, ")")
+                clear = 0;
+
+if clear:
+    print("All good!")
+    
